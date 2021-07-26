83d67586fd8235aaf7731814b1104f4c9acc019e
Agam Singh <singh.agam0009@gmail.com>
2021-07-26 14:20:28 +1000

    First

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ExecptionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Enter Room Number: ");
+                string room = Console.ReadLine();
+
+                if (room.Trim() == String.Empty)
+                throw new ArgumentException("Input cannot be empty", "Enter Room Number");
+
+
+                System.Console.WriteLine("Enter Name: ");
+                string name = Console.ReadLine();
+
+                if (name.Trim() == String.Empty)
+                throw new ArgumentException("Input cannot be empty", "Enter Name");
+
+            try {
+
+                System.Console.WriteLine("Enter Date: Day");
+                int day = Int32.Parse(Console.ReadLine()); 
+
+                System.Console.WriteLine("Enter Date: Month");
+                int month = Int32.Parse(Console.ReadLine());
+
+                System.Console.WriteLine("Enter Date: Year");
+                int year = Int32.Parse(Console.ReadLine());
+
+                System.Console.WriteLine("Enter Time");
+                double Time = Int32.Parse(Console.ReadLine());
+
+                System.Console.WriteLine("Enter amount of time student stayed in the room (IN MINUTES): ");
+                int time = Int32.Parse(Console.ReadLine());  
+            
+            }
+            
+            catch (FormatException e) {
+                System.Console.WriteLine($"Date - Time only uses numbers - {e.Message}");
+                }
+
+            catch (NullReferenceException ex) {
+                System.Console.WriteLine($"Object doesn't exist :( Closing Program - {ex.Message}");
+            }
+            catch (Exception ex) {
+                System.Console.WriteLine($"Something went wrong :( Closing Program - {ex.Message}");
+            }
+            
+        }
+    }
+}
